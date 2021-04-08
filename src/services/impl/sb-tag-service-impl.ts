483f40c059d4f22c73739a63c3671e34398c09c0
sharath <sharaths.kashyap@gmail.com>
2021-04-08 15:54:49 +0530

    Issue #SB-23812 Feat: Segmentation as a Capability

--- src/services/impl/sb-tag-service-impl.ts
+++ src/services/impl/sb-tag-service-impl.ts
@@ -12,7 +12,6 @@ export class SBTagServiceImpl implements SBTagService {
         let tagArray = Object.values(result);
         let prefixTagArr = tagArray.map(i => prefix.toString() + i)
         this.__tagSnapShot[prefix.toString()] = prefixTagArr; 
-        //this.__tagObj[prefix.toString()] = result;
         Object.assign(this.__tagObj[prefix.toString()],result);
         this.calculateTags();
     }    
@@ -40,6 +39,9 @@ export class SBTagServiceImpl implements SBTagService {
     getTagAttributes() {
         return this.propertiesToArray(this.__tagObj);
     }
+    getTagAttributeValues() {
+        return this.propertiesToArrayKeyValues(this.__tagObj);
+    }
 
     appendTag(result: any, prefix: String, deep?: Boolean | undefined) {
         if(result instanceof Object) {
@@ -62,7 +64,7 @@ export class SBTagServiceImpl implements SBTagService {
         }
     }
 
-    propertiesToArray(obj: any) {
+    private propertiesToArray(obj: any) {
         const isObject = val =>
             typeof val === 'object' && !Array.isArray(val);
     
@@ -83,4 +85,20 @@ export class SBTagServiceImpl implements SBTagService {
         return paths(obj);
     }
 
+    private propertiesToArrayKeyValues(obj: any, prefix?) {
+
+        var res = {};
+    
+        for (var k of Object.keys(obj)) {
+            var val = obj[k],
+                key = prefix ? prefix + '.' + k : k;
+    
+            if (typeof val === 'object')
+                Object.assign(res, this.propertiesToArrayKeyValues(val, key)); // <-- recursion
+            else
+                res[key] = val;
+        }
+    
+        return res;
+    }
 }
